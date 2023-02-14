b2e0fd1e3ca3f295a46ff22e68640812dec4213c
beginerbeginer <miyabi6125@gmail.com>
2023-02-14 17:28:38 +0900

    実行：Eslint&Prettier

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,31 @@
-import React from "react";
-import { useCookies } from "react-cookie";
-import { useSelector, useDispatch } from "react-redux/es/exports";
-import { useNavigate } from "react-router-dom";
-import  { signOut } from "../authSlice";
-import "./header.css";
+import React from 'react'
+import { useCookies } from 'react-cookie'
+import { useSelector, useDispatch } from 'react-redux/es/exports'
+import { useNavigate } from 'react-router-dom'
+import { signOut } from '../authSlice'
+import './header.css'
 
 export const Header = () => {
   const auth = useSelector((state) => state.auth.isSignIn)
-  const dispatch = useDispatch();
-  const navigation = useNavigate();
-  const [cookies, setCookie, removeCookie] = useCookies();
+  const dispatch = useDispatch()
+  const navigation = useNavigate()
+  const [removeCookie] = useCookies()
   const handleSignOut = () => {
-    dispatch(signOut());
-    removeCookie("token");
-    navigation("/signin");
+    dispatch(signOut())
+    removeCookie('token')
+    navigation('/signin')
   }
 
   return (
     <header className="header">
       <h1>Todoアプリ</h1>
-      {auth ? <button onClick={handleSignOut} className="sign-out-button">サインアウト</button> : <></>}
+      {auth ? (
+        <button onClick={handleSignOut} className="sign-out-button">
+          サインアウト
+        </button>
+      ) : (
+        <></>
+      )}
     </header>
   )
 }
