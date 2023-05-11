44108f685bd78795c7297905e84ce6fddd4ca25c
beginerbeginer <miyabi6125@gmail.com>
2023-05-11 16:49:52 +0900

    修正：DOMの操作をuseRefに変更

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,10 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 // 表示するタスク
 export const Tasks = (props) => {
   const { tasks, selectListId, isDoneDisplay, getRemainingTime } = props
   const [focusedTaskId, setFocusedTaskId] = useState('')
+  const taskRefs = useRef({})
+
+  useEffect(() => {
+    if (focusedTaskId && taskRefs.current[focusedTaskId]) {
+      taskRefs.current[focusedTaskId].focus()
+    }
+  }, [focusedTaskId])
 
   if (tasks === null) return <></>
 
@@ -27,8 +34,9 @@ export const Tasks = (props) => {
         newIndex = currentIndex + 1
         break
       case 'Enter': {
-        const link = document.getElementById(`task-link-${focusedTaskId}`)
-        link.click()
+        if (taskRefs.current[focusedTaskId]) {
+          taskRefs.current[focusedTaskId].click()
+        }
         return
       }
       default:
@@ -37,8 +45,6 @@ export const Tasks = (props) => {
 
     if (newIndex >= 0 && newIndex < filteredTasks.length) {
       setFocusedTaskId(taskIds[newIndex])
-      const nextListItem = document.getElementById(`task-${taskIds[newIndex]}`)
-      nextListItem.focus()
     }
   }
 
@@ -46,14 +52,13 @@ export const Tasks = (props) => {
     <ul tabIndex={0} onKeyDown={handleTaskNavigation}>
       {filteredTasks.map((task, key) => {
         return (
-          <li
-            key={key}
-            id={`task-${task.id}`}
-            className="task-item"
-            tabIndex={0}
-            onFocus={() => setFocusedTaskId(task.id)}
-          >
-            <Link to={`/lists/${selectListId}/tasks/${task.id}`} className="task-item-link" id={`task-link-${task.id}`}>
+          <li key={key} className="task-item" tabIndex={0} onFocus={() => setFocusedTaskId(task.id)}>
+            <Link
+              to={`/lists/${selectListId}/tasks/${task.id}`}
+              className="task-item-link"
+              id={`task-link-${task.id}`}
+              ref={(el) => (taskRefs.current[task.id] = el)}
+            >
               {task.title}
               <br />
               {task.done ? '完了' : '未完了'}, {getRemainingTime(task.limit)}
